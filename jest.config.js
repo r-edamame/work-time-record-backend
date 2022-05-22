c027c3793707cfdef94ee04cbf82d168f68908b3
edamame <edamame15.r@gmail.com>
2022-05-22 20:15:46 +0900

    install jest

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  "roots": [
+    "<rootDir>/src"
+  ],
+  "testMatch": [
+    "**/__tests__/**/*.+(ts|tsx|js)",
+    "**/?(*.)+(spec|test).+(ts|tsx|js)"
+  ],
+  "transform": {
+    "^.+\\.(ts|tsx)$": "ts-jest"
+  },
+  "moduleNameMapper": {
+    "^@domain/(.*)$": "<rootDir>/src/domain/$1",
+    "^@util/(.*)$": "<rootDir>/src/util/$1",
+  }
+}
